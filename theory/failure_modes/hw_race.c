4a70a4577e0b726bbe9ec012ed5d1fe1cd4388e6
Jonah Broh <Jonah Broh>
2017-09-27 15:39:15 -0600

    more

--- theory/failure_modes/hw_race.c
+++ theory/failure_modes/hw_race.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
+omp_lock_t lockA;
 int main(int argc, char** argv) {
     size_t T=8;
     int t_hello[T];
@@ -12,8 +13,11 @@ int main(int argc, char** argv) {
     i=0;
     #pragma omp parallel num_threads(8)
     {
+          omp_init_lock(&lockA);
+          omp_set_lock(&lockA);
           t_hello[i]=omp_get_thread_num();
           i++;
+          omp_unset_lock(&lockA);
           while (i < T){
             printf("%d\n", i);
             sleep(1);
