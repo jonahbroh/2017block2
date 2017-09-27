7f7d655e1d0413e5d1f12fe80e97c56858f4855d
Jonah Broh <Jonah Broh>
2017-09-27 13:39:20 -0600

    more

--- theory/failure_modes/hw_race.c
+++ theory/failure_modes/hw_race.c
@@ -9,10 +9,13 @@ int main(int argc, char** argv) {
         t_hello[i]=-1;
     }
     i=0;
+    omp_init_lock(&lockA);
     #pragma omp parallel num_threads(8)
     {
+        omp_set_lock(&lockA);
         t_hello[i]=omp_get_thread_num();
         i++;
+        omp_unset_lock(&lockA);
     }
 
     for(i=0;i<T;i++) {
