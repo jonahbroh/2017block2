12172c7c68c1cfa5ea201e4324b831a43c9517ec
Jonah Broh <Jonah Broh>
2017-10-04 01:00:34 -0600

    reorg?

--- patterns/map/cilk.c
+++ patterns/map/cilk.c
@@ -84,7 +84,7 @@ int main(int argc, char** argv) {
     clock_gettime(CLOCK_MONOTONIC,&start_time);
     // While a match has not been found, search
     int notfound=1;
-    cilk_for(int i = 0; i<99999999; i++){
+    for(int i = 0; i<99999999; i++){
       // generate the password
       currpass = cilk_spawn gentest(i, passmatch, argv[1]);
       if (currpass < 0){
