e886de08cfe940c10bf43c9a0b29e3489a466937
Jonah Broh <Jonah Broh>
2017-10-04 11:33:30 -0600

    stencil?

--- patterns/stencil/openmp.cpp
+++ patterns/stencil/openmp.cpp
@@ -94,10 +94,17 @@ void gaussian_kernel(const int rows, const int cols, const double stddev, double
 /*
  * Applies a gaussian blur stencil to an image
  */
-void apply_stencil(const int radius, const double stddev, const int rows, const int cols, pixel * const in, pixel * const out) {
+void apply_stencil(const int radius, const double stddev, const int rows, const int cols, pixel * const in, pixel * const out, int stencil) {
     const int dim = radius*2+1;
     double kernel[dim*dim];
-    gaussian_kernel(dim, dim, stddev, kernel);
+    double kernel2[dim*dim];
+    if stencil == 0{
+      gaussian_kernel(dim, dim, stddev, kernel);
+    }
+    else{
+      prewittX_kernel(dim, dim, kernel);
+      prewittY_kernel(dim, dim, kernel2);
+    }
     // For each pixel in the image...
     #pragma omp parallel for
     for(int i = 0; i < rows; ++i) {
@@ -107,14 +114,28 @@ void apply_stencil(const int radius, const double stddev, const int rows, const 
             for(int x = i - radius, kx = 0; x <= i + radius; ++x, ++kx) {
                 for(int y = j - radius, ky = 0; y <= j + radius; ++y, ++ky) {
                     // ...and skip parts of the template outside of the image
-                    // #pragma omp task depend(in:in) depend(out:out)
                     if(x >= 0 && x < rows && y >= 0 && y < cols) {
                         // Acculate intensities in the output pixel
                         const int in_offset = x + (y*rows);
                         const int k_offset = kx + (ky*dim);
-                        out[out_offset].red   += kernel[k_offset] * in[in_offset].red;
-                        out[out_offset].green += kernel[k_offset] * in[in_offset].green;
-                        out[out_offset].blue  += kernel[k_offset] * in[in_offset].blue;
+                        if(stencil == 0){
+                          out[out_offset].red   += kernel[k_offset] * in[in_offset].red;
+                          out[out_offset].green += kernel[k_offset] * in[in_offset].green;
+                          out[out_offset].blue  += kernel[k_offset] * in[in_offset].blue;
+                          // double intensity = (out[out_offset].red + out[out_offset].green + out[out_offset].blue)/3.0;
+                          // out[out_offset].red = intensity;
+                          // out[out_offset].green = intensity;
+                          // out[out_offset].blue  = intensity;
+                        }
+                        else{
+                          double intensity = (in[in_offset].red + in[in_offset].green + in[in_offset].blue)/3.0;
+                          double xEdge = kernel[k_offset] * intensity;
+                          double yEdge = kernel2[k_offset] * intensity;
+                          double outIntensity = sqrt(xEdge*xEdge + yEdge*yEdge);
+                          out[out_offset].red = outIntensity;
+                          out[out_offset].green = outIntensity;
+                          out[out_offset].blue = outIntensity;
+                        }
                     }
                 }
             }
@@ -148,19 +169,28 @@ int main( int argc, char* argv[] ) {
         }
     }
 
-    // Create output array
-    pixel * outPixels = (pixel *) malloc(rows * cols * sizeof(pixel));
+    // Create output array 1
+    pixel * outPixels1 = (pixel *) malloc(rows * cols * sizeof(pixel));
     for(int i = 0; i < rows * cols; ++i) {
-        outPixels[i].red = 0.0;
-        outPixels[i].green = 0.0;
-        outPixels[i].blue = 0.0;
+        outPixels1[i].red = 0.0;
+        outPixels1[i].green = 0.0;
+        outPixels1[i].blue = 0.0;
+    }
+
+    // Create output array 2
+    pixel * outPixels2 = (pixel *) malloc(rows * cols * sizeof(pixel));
+    for(int i = 0; i < rows * cols; ++i) {
+        outPixels2[i].red = 0.0;
+        outPixels2[i].green = 0.0;
+        outPixels2[i].blue = 0.0;
     }
 
     // Do the stencil
     struct timespec start_time;
     struct timespec end_time;
     clock_gettime(CLOCK_MONOTONIC,&start_time);
-    apply_stencil(3, 32.0, rows, cols, imagePixels, outPixels);
+    apply_stencil(3, 32.0, rows, cols, imagePixels, outPixels1, 0);
+    apply_stencil(3, 32.0, rows, cols, outPixels1, outPixels2, 1);
     clock_gettime(CLOCK_MONOTONIC,&end_time);
     long msec = (end_time.tv_sec - start_time.tv_sec)*1000 + (end_time.tv_nsec - start_time.tv_nsec)/1000000;
     printf("Stencil application took %dms\n",msec);
