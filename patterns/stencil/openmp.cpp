76ef295f3c89a47af92ba9b7d3e8faf530bd93f3
Jonah Broh <Jonah Broh>
2017-10-04 11:01:19 -0600

    reorg?

--- patterns/stencil/openmp.cpp
+++ patterns/stencil/openmp.cpp
@@ -99,6 +99,7 @@ void apply_stencil(const int radius, const double stddev, const int rows, const 
     double kernel[dim*dim];
     gaussian_kernel(dim, dim, stddev, kernel);
     // For each pixel in the image...
+    #pragma omp parallel for
     for(int i = 0; i < rows; ++i) {
         for(int j = 0; j < cols; ++j) {
             const int out_offset = i + (j*rows);
@@ -106,6 +107,7 @@ void apply_stencil(const int radius, const double stddev, const int rows, const 
             for(int x = i - radius, kx = 0; x <= i + radius; ++x, ++kx) {
                 for(int y = j - radius, ky = 0; y <= j + radius; ++y, ++ky) {
                     // ...and skip parts of the template outside of the image
+                    #pragma omp task depend(in:in[in_offset]) depend(out:out[out_offset])
                     if(x >= 0 && x < rows && y >= 0 && y < cols) {
                         // Acculate intensities in the output pixel
                         const int in_offset = x + (y*rows);
