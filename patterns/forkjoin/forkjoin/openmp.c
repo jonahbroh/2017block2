6c22ed8a4ea6b14cb62d0321287544371ba15ab7
Jonah Broh <Jonah Broh>
2017-10-06 11:35:54 -0600

    forkjoin?

--- patterns/forkjoin/forkjoin/openmp.c
+++ patterns/forkjoin/forkjoin/openmp.c
@@ -34,7 +34,7 @@ int main(int argc, char** argv) {
     clock_gettime(CLOCK_MONOTONIC,&start_time);
     #pragma omp parallel
     {
-      // #pragma omp single
+      #pragma omp single
       {
         out = q(n);
       }
