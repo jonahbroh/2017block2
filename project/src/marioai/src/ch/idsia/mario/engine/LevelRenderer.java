605d88234ffffc28c8fb11f030de7c646de8a211
Jonah Broh <Jonah Broh>
2017-10-17 17:39:55 -0600

    project

--- project/src/marioai/src/ch/idsia/mario/engine/LevelRenderer.java
+++ project/src/marioai/src/ch/idsia/mario/engine/LevelRenderer.java
@@ -71,106 +71,117 @@ public class LevelRenderer
 
     private void updateArea(int x0, int y0, int w, int h)
     {
-        g.setBackground(transparent);
-        g.clearRect(x0, y0, w, h);
-        int xTileStart = (x0 + xCam) / 16;
-        int yTileStart = (y0 + yCam) / 16;
-        int xTileEnd = (x0 + xCam + w) / 16;
-        int yTileEnd = (y0 + yCam + h) / 16;
-        for (int x = xTileStart; x <= xTileEnd; x++)
-        {
-            for (int y = yTileStart; y <= yTileEnd; y++)
-            {
-                int b = level.getBlock(x, y) & 0xff;
-                if (((Level.TILE_BEHAVIORS[b]) & Level.BIT_ANIMATED) == 0)
-                {
-                    g.drawImage(Art.level[b % 16][b / 16], (x << 4) - xCam, (y << 4) - yCam, null);
-                }
-            }
+
+        GraphicsEnvironment ge =
+        GraphicsEnvironment.getLocalGraphicsEnvironment();
+        boolean headless_check = ge.isHeadless();
+        if(!headless_check){
+          g.setBackground(transparent);
+          g.clearRect(x0, y0, w, h);
+          int xTileStart = (x0 + xCam) / 16;
+          int yTileStart = (y0 + yCam) / 16;
+          int xTileEnd = (x0 + xCam + w) / 16;
+          int yTileEnd = (y0 + yCam + h) / 16;
+          for (int x = xTileStart; x <= xTileEnd; x++)
+          {
+              for (int y = yTileStart; y <= yTileEnd; y++)
+              {
+                  int b = level.getBlock(x, y) & 0xff;
+                  if (((Level.TILE_BEHAVIORS[b]) & Level.BIT_ANIMATED) == 0)
+                  {
+                      g.drawImage(Art.level[b % 16][b / 16], (x << 4) - xCam, (y << 4) - yCam, null);
+                  }
+              }
+          }
         }
     }
 
     public void render(Graphics g, int tick, float alpha)
     {
-        g.drawImage(image, 0, 0, null);
+        GraphicsEnvironment ge =
+        GraphicsEnvironment.getLocalGraphicsEnvironment();
+        boolean headless_check = ge.isHeadless();
+        if(!headless_check){
+          g.drawImage(image, 0, 0, null);
 
-        for (int x = xCam / 16; x <= (xCam + width) / 16; x++)
-            for (int y = yCam / 16; y <= (yCam + height) / 16; y++)
-            {
-                byte b = level.getBlock(x, y);
+          for (int x = xCam / 16; x <= (xCam + width) / 16; x++)
+              for (int y = yCam / 16; y <= (yCam + height) / 16; y++)
+              {
+                  byte b = level.getBlock(x, y);
 
-                if (((Level.TILE_BEHAVIORS[b & 0xff]) & Level.BIT_ANIMATED) > 0)
-                {
-                    int animTime = (tick / 3) % 4;
+                  if (((Level.TILE_BEHAVIORS[b & 0xff]) & Level.BIT_ANIMATED) > 0)
+                  {
+                      int animTime = (tick / 3) % 4;
 
-                    if ((b % 16) / 4 == 0 && b / 16 == 1)
-                    {
-                        animTime = (tick / 2 + (x + y) / 8) % 20;
-                        if (animTime > 3) animTime = 0;
-                    }
-                    if ((b % 16) / 4 == 3 && b / 16 == 0)
-                    {
-                        animTime = 2;
-                    }
-                    int yo = 0;
-                    if (x >= 0 && y >= 0 && x < level.width && y < level.height) yo = level.data[x][y];
-                    if (yo > 0) yo = (int) (Math.sin((yo - alpha) / 4.0f * Math.PI) * 8);
-                    g.drawImage(Art.level[(b % 16) / 4 * 4 + animTime][b / 16], (x << 4) - xCam, (y << 4) - yCam - yo, null);
-                }
-                /*                else if (b == Level.TILE_BONUS)
-                 {
-                 int animTime = (tick / 3) % 4;
-                 int yo = 0;
-                 if (x >= 0 && y >= 0 && x < level.width && y < level.height) yo = level.data[x][y];
-                 if (yo > 0) yo = (int) (Math.sin((yo - alpha) / 4.0f * Math.PI) * 8);
-                 g.drawImage(Art.mapSprites[(4 + animTime)][0], (x << 4) - xCam, (y << 4) - yCam - yo, null);
-                 }*/
+                      if ((b % 16) / 4 == 0 && b / 16 == 1)
+                      {
+                          animTime = (tick / 2 + (x + y) / 8) % 20;
+                          if (animTime > 3) animTime = 0;
+                      }
+                      if ((b % 16) / 4 == 3 && b / 16 == 0)
+                      {
+                          animTime = 2;
+                      }
+                      int yo = 0;
+                      if (x >= 0 && y >= 0 && x < level.width && y < level.height) yo = level.data[x][y];
+                      if (yo > 0) yo = (int) (Math.sin((yo - alpha) / 4.0f * Math.PI) * 8);
+                      g.drawImage(Art.level[(b % 16) / 4 * 4 + animTime][b / 16], (x << 4) - xCam, (y << 4) - yCam - yo, null);
+                  }
+                  /*                else if (b == Level.TILE_BONUS)
+                   {
+                   int animTime = (tick / 3) % 4;
+                   int yo = 0;
+                   if (x >= 0 && y >= 0 && x < level.width && y < level.height) yo = level.data[x][y];
+                   if (yo > 0) yo = (int) (Math.sin((yo - alpha) / 4.0f * Math.PI) * 8);
+                   g.drawImage(Art.mapSprites[(4 + animTime)][0], (x << 4) - xCam, (y << 4) - yCam - yo, null);
+                   }*/
 
-                if (renderBehaviors)
-                {
-                    if (((Level.TILE_BEHAVIORS[b & 0xff]) & Level.BIT_BLOCK_UPPER) > 0)
-                    {
-                        g.setColor(Color.RED);
-                        g.fillRect((x << 4) - xCam, (y << 4) - yCam, 16, 2);
-                    }
-                    if (((Level.TILE_BEHAVIORS[b & 0xff]) & Level.BIT_BLOCK_ALL) > 0)
-                    {
-                        g.setColor(Color.RED);
-                        g.fillRect((x << 4) - xCam, (y << 4) - yCam, 16, 2);
-                        g.fillRect((x << 4) - xCam, (y << 4) - yCam + 14, 16, 2);
-                        g.fillRect((x << 4) - xCam, (y << 4) - yCam, 2, 16);
-                        g.fillRect((x << 4) - xCam + 14, (y << 4) - yCam, 2, 16);
-                    }
-                    if (((Level.TILE_BEHAVIORS[b & 0xff]) & Level.BIT_BLOCK_LOWER) > 0)
-                    {
-                        g.setColor(Color.RED);
-                        g.fillRect((x << 4) - xCam, (y << 4) - yCam + 14, 16, 2);
-                    }
-                    if (((Level.TILE_BEHAVIORS[b & 0xff]) & Level.BIT_SPECIAL) > 0)
-                    {
-                        g.setColor(Color.PINK);
-                        g.fillRect((x << 4) - xCam + 2 + 4, (y << 4) - yCam + 2 + 4, 4, 4);
-                    }
-                    if (((Level.TILE_BEHAVIORS[b & 0xff]) & Level.BIT_BUMPABLE) > 0)
-                    {
-                        g.setColor(Color.BLUE);
-                        g.fillRect((x << 4) - xCam + 2, (y << 4) - yCam + 2, 4, 4);
-                    }
-                    if (((Level.TILE_BEHAVIORS[b & 0xff]) & Level.BIT_BREAKABLE) > 0)
-                    {
-                        g.setColor(Color.GREEN);
-                        g.fillRect((x << 4) - xCam + 2 + 4, (y << 4) - yCam + 2, 4, 4);
-                    }
-                    if (((Level.TILE_BEHAVIORS[b & 0xff]) & Level.BIT_PICKUPABLE) > 0)
-                    {
-                        g.setColor(Color.YELLOW);
-                        g.fillRect((x << 4) - xCam + 2, (y << 4) - yCam + 2 + 4, 4, 4);
-                    }
-                    if (((Level.TILE_BEHAVIORS[b & 0xff]) & Level.BIT_ANIMATED) > 0)
-                    {
-                    }
-                }
+                  if (renderBehaviors)
+                  {
+                      if (((Level.TILE_BEHAVIORS[b & 0xff]) & Level.BIT_BLOCK_UPPER) > 0)
+                      {
+                          g.setColor(Color.RED);
+                          g.fillRect((x << 4) - xCam, (y << 4) - yCam, 16, 2);
+                      }
+                      if (((Level.TILE_BEHAVIORS[b & 0xff]) & Level.BIT_BLOCK_ALL) > 0)
+                      {
+                          g.setColor(Color.RED);
+                          g.fillRect((x << 4) - xCam, (y << 4) - yCam, 16, 2);
+                          g.fillRect((x << 4) - xCam, (y << 4) - yCam + 14, 16, 2);
+                          g.fillRect((x << 4) - xCam, (y << 4) - yCam, 2, 16);
+                          g.fillRect((x << 4) - xCam + 14, (y << 4) - yCam, 2, 16);
+                      }
+                      if (((Level.TILE_BEHAVIORS[b & 0xff]) & Level.BIT_BLOCK_LOWER) > 0)
+                      {
+                          g.setColor(Color.RED);
+                          g.fillRect((x << 4) - xCam, (y << 4) - yCam + 14, 16, 2);
+                      }
+                      if (((Level.TILE_BEHAVIORS[b & 0xff]) & Level.BIT_SPECIAL) > 0)
+                      {
+                          g.setColor(Color.PINK);
+                          g.fillRect((x << 4) - xCam + 2 + 4, (y << 4) - yCam + 2 + 4, 4, 4);
+                      }
+                      if (((Level.TILE_BEHAVIORS[b & 0xff]) & Level.BIT_BUMPABLE) > 0)
+                      {
+                          g.setColor(Color.BLUE);
+                          g.fillRect((x << 4) - xCam + 2, (y << 4) - yCam + 2, 4, 4);
+                      }
+                      if (((Level.TILE_BEHAVIORS[b & 0xff]) & Level.BIT_BREAKABLE) > 0)
+                      {
+                          g.setColor(Color.GREEN);
+                          g.fillRect((x << 4) - xCam + 2 + 4, (y << 4) - yCam + 2, 4, 4);
+                      }
+                      if (((Level.TILE_BEHAVIORS[b & 0xff]) & Level.BIT_PICKUPABLE) > 0)
+                      {
+                          g.setColor(Color.YELLOW);
+                          g.fillRect((x << 4) - xCam + 2, (y << 4) - yCam + 2 + 4, 4, 4);
+                      }
+                      if (((Level.TILE_BEHAVIORS[b & 0xff]) & Level.BIT_ANIMATED) > 0)
+                      {
+                      }
+                  }
 
+              }
             }
     }
 
