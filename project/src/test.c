a5866f6a577528fc2103f30904bc70f0d86d2de6
Jonah Broh <Jonah Broh>
2017-10-17 18:14:40 -0600

    project

--- project/src/test.c
+++ project/src/test.c
@@ -7,5 +7,5 @@ int main(){
   char* javapath = "/modules/packages/jdk-9/bin/java";
   char* gamepath = "/home/nfs/j_broh/2017block2/project/src/marioai/classes";
   char* classname = "ch.idsia.scenarios.Play";
-  execl(javapath, javapath, "-cp", gamepath, classname, "1", i, chromosome_string(pop.agents[i]));
+  execl(javapath, javapath, "-cp", gamepath, classname, "1", "999", "1111111222222233333330000000");
 }
