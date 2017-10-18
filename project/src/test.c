e24deed71ab3437297c8c9a95d201f75e777d98e
Jonah Broh <Jonah Broh>
2017-10-17 18:33:37 -0600

    project

--- project/src/test.c
+++ project/src/test.c
@@ -7,13 +7,12 @@ int main(){
   char* javapath = "/modules/packages/jdk-9/bin/java";
   char* gamepath = "/home/nfs/j_broh/2017block2/project/src/marioai/classes";
   char* classname = "ch.idsia.scenarios.Play";
-  FILE* fit;
   char fitpath[100];
   char* fitdir = "./marioai/scores/fitness";
-  char fitstr[100];
+  char fitstr[1000];
   sprintf(fitpath, "%s%d", fitdir, 999);
-  fit = fopen(fitpath, "r");
-  fgets(fitstr, 100, fit);
+  FILE *fit = fopen(fitpath, "r");
+  fgets(fitstr, 1000, fit);
   printf(fitstr);
 
   // execl(javapath, javapath, "-cp", gamepath, classname, "1", "999", "1111111222222233333330000000");
