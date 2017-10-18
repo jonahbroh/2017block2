501e1c2b6398d599f5b75c0c29717f8e848ee346
Jonah Broh <Jonah Broh>
2017-10-17 18:07:58 -0600

    project

--- project/src/test.c
+++ project/src/test.c
@@ -1,4 +1,8 @@
 int main(){
+  #include <stdio.h>
+  #include <stdlib.h>
+  #include <unistd.h>
+  #include <sys/wait.h>
   char* javapath = "/home/nfs/j_broh/2017block2/project/src/marioai/classes";
   char* classname = "ch.idsia.scenarios.Play";
   execl(javapath, "java", classname, "1", "999", "1111111222222233333330000000");
