fa7ee05519d07dcdb7bc3634b323dde39bac6959
Jonah Broh <Jonah Broh>
2017-10-18 08:11:11 -0600

    morning

--- project/src/ga.c
+++ project/src/ga.c
@@ -173,12 +173,12 @@ int main(int argc, char** argv) {
     clock_gettime(CLOCK_MONOTONIC,&end_time);
     long msec = (end_time.tv_sec - start_time.tv_sec)*1000 + (end_time.tv_nsec - start_time.tv_nsec)/1000000;
     printf("Generation %d, top fitness %d, %d msec\n", i, pop.agents[pop_size - 1].fitness, msec);
-    if(pop.agents[0].fitness >= 4000){
-      printf("Fitness: %d Generations: %d", pop.agents[0].fitness, i);
+    if(pop.agents[pop_size - 1].fitness >= 4000){
+      printf("Fitness: %d Generations: %d", pop.agents[pop_size - 1].fitness, i);
       return i;
     }
     pop = new_population(pop, crossover_rate);
   }
-  printf("Fitness: %d Generations: %d", pop.agents[0].fitness, 500);
+  printf("Fitness: %d Generations: %d", pop.agents[pop_size - 1].fitness, 500);
   return 500;
 }
