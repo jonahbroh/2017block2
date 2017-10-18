7053c297807c54267d89a600adbebeb9631412f6
Jonah Broh <Jonah Broh>
2017-10-17 18:35:31 -0600

    project

--- project/src/test.c
+++ project/src/test.c
@@ -11,7 +11,7 @@ int main(){
   char* fitdir = "./marioai/scores/fitness";
   char fitstr[1000];
   sprintf(fitpath, "%s%d", fitdir, 999);
-  printf(fitpath + "\n");
+  printf("%s\n", fitpath);
   FILE *fit = fopen(fitpath, "r");
   fgets(fitstr, 1000, fit);
   printf(fitstr);
